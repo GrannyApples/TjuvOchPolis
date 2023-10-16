a620e1c66a33aee1761f87b080b9f46f21ff1a99
GrannyApples <anton.engstrom1994@gmail.com>
2023-10-16 14:36:29 +0200

    UpdatesPerson

--- TjuvOPolis/TjuvOPolis/Person.cs
+++ TjuvOPolis/TjuvOPolis/Person.cs
@@ -8,5 +8,17 @@ namespace TjuvOPolis
 {
     internal class Person
     {
+        public string Polis { get; set; }
+        public string Tjuv { get; set; }
+        public string Medborgare { get; set; }
+
+        public Person(string polis,string tjuv,string medborgare)
+
+        {
+            Medborgare = medborgare;
+            Polis = polis;  
+            Tjuv = tjuv;
+            
+        }
     }
 }
