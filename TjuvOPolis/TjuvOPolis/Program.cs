4e8ed3ac591fa343b3e5e2da05a7e8c9ddba02ea
GrannyApples <anton.engstrom1994@gmail.com>
2023-10-16 14:20:30 +0200

    Hej

---
+++ TjuvOPolis/TjuvOPolis/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TjuvOPolis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hej");
+        }
+    }
+}
