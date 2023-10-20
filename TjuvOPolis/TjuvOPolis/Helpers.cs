7cbcd42d5acfcbd2aa4930ff5078ad6cea9cc65e
Tallis92 <147599891+Tallis92@users.noreply.github.com>
2023-10-20 14:21:34 +0200

    Fredag eftereftermiddag
    Lagt till If satser för kollisioner och lite annat.

--- TjuvOPolis/TjuvOPolis/Helpers.cs
+++ TjuvOPolis/TjuvOPolis/Helpers.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TjuvOPolis
+﻿namespace TjuvOPolis
 {
     public class Helpers
     {
@@ -22,7 +16,7 @@ namespace TjuvOPolis
                 person.MovementX = person.MovementX + moveDirectionX;
                 person.MovementY = person.MovementY + moveDirectionY;
 
-                
+
                 movementXY[0] = person.MovementX;
                 movementXY[1] = person.MovementY;
 
@@ -43,14 +37,14 @@ namespace TjuvOPolis
                 if (person.MovementY == 99)
                 {
                     person.MovementY = 1;
-                }   
+                }
             }
             return movementXY;
         }
         public static List<Person> FillPeople(int population, int numOfPolice, int numOfCriminals)
         {
             List<Person> list = new List<Person>();
-            
+
             for (int i = 0; i < population; i++)
             {
                 List<string> inventory = Helpers.FillInventory();
@@ -76,10 +70,10 @@ namespace TjuvOPolis
         public static List<string> FillInventory()
         {
             List<string> inventory = new List<string>();
-            inventory.Add("Wallet");
-            inventory.Add("Watch");
-            inventory.Add("Keys");
-            inventory.Add("Phone");
+            inventory.Add("plånbok");
+            inventory.Add("klocka");
+            inventory.Add("nycklar");
+            inventory.Add("telefon");
 
             return inventory;
         }
@@ -94,33 +88,65 @@ namespace TjuvOPolis
                     {
                         if (people[i].Marker == "M" && people[j].Marker == "T")
                         {
-                            
+
                             collisions[people[i].MovementX, people[i].MovementY] = "TM";
-                            Thread.Sleep(5000);
+
                             Console.WriteLine("Tjuv och medborgare kolliderar vid: " + people[i].MovementX + "." + people[i].MovementY);
-                                for (int k = 0; k < ((Citizen)people[i]).Inventory.Count; k++)
-                                {
-                                    Console.Write(((Citizen)people[i]).Inventory[k] + " ");
-                                    
+                            Random rnd = new Random();
+                            int stealIndex = rnd.Next(0, ((Citizen)people[i]).Inventory.Count);
+                            
+                            if (((Citizen)people[i]).Inventory.Count == 0)
+                            {
+                                Console.WriteLine("Medborgare #" + i + " har inga föremål kvar. Tjuv #" + j + " är väldigt besviken.");
+                                
                             }
+                            else
+                            {
+                                //Lägger till föremål från citizens Index till tjuvens inventory
+                                //Sedan tar bort föremål från citizens Index från inventory
+                                Console.WriteLine("Tjuv #" + j + " har tagit Medborgare #" + i + "'s " + ((Citizen)people[i]).Inventory[stealIndex]);
+                                ((Thief)people[j]).ThiefInventory.Add(((Citizen)people[i]).Inventory[stealIndex]);
+                                ((Citizen)people[i]).Inventory.RemoveAt(stealIndex);
+                            }
+                            
+
+                            Console.WriteLine();
+
+                            Thread.Sleep(2000);
 
                         }
                         if (people[i].Marker == "P" && people[j].Marker == "T")
                         {
                             collisions[people[i].MovementX, people[i].MovementY] = "TP";
                             Console.WriteLine("Tjuv och polis kolliderar vid: " + people[i].MovementX + "." + people[i].MovementY);
+
+                            if (((Thief)people[j]).ThiefInventory.Count > 0)
+                            {
+                                Console.WriteLine("Polis #" + i + " har haffat Tjuv # " + j + " och har beslagtagit allt stöldgods och skickat tjuven till fängelset.");
+                                for (int p = 0; p < ((Thief)people[j]).ThiefInventory.Count; p++)
+                                {
+                                    ((Police)people[i]).PoliceInventory.Add(((Thief)people[j]).ThiefInventory[p]);
+                                    ((Thief)people[j]).ThiefInventory.RemoveAt(p);
+                                }
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                
+                            }
+                            
                         }
                         if (people[i].Marker == "M" && people[j].Marker == "P")
                         {
                             collisions[people[i].MovementX, people[i].MovementY] = "MP";
                             Console.WriteLine("Medborgare och polis kolliderar vid: " + people[i].MovementX + "." + people[i].MovementY);
                         }
-                        
+
                     }
-                    
+
                 }
             }
-           
+
             return collisions;
         }
 
--- TjuvOPolis/TjuvOPolis/Program.cs
+++ TjuvOPolis/TjuvOPolis/Program.cs
@@ -8,8 +8,8 @@
             string[,] collisions = new string[25, 100];
             //string[] backpack = 
             int population = 20;
-            int thiefPopulation = 20;
-            int policePopulation =0;
+            int thiefPopulation = 10;
+            int policePopulation =30;
 
             List<Person> people = Helpers.FillPeople(population, policePopulation, thiefPopulation);
 
@@ -18,9 +18,10 @@
             while (true)
             {
                 Helpers.Mover(people);
-                collisions = Helpers.Collision(people, population, collisions);
+               
                 City.CityDrawer(city, people, collisions);
-                Thread.Sleep(1100);
+                collisions = Helpers.Collision(people, population, collisions);
+                Thread.Sleep(100);
                 Array.Clear(collisions, 0, collisions.Length);
                 
                 Console.Clear();
